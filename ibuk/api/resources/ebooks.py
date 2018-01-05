1b892065dc28ef008b830960cb1a32aded96f558
Alberto Curro <acurrope@cisco.com>
2018-01-05 17:29:57 +0000

    Imperfect but promising dynamic filtering from json

--- ibuk/api/resources/ebooks.py
+++ ibuk/api/resources/ebooks.py
@@ -1,26 +1,30 @@
 from collections import namedtuple
 from flask_restful import Resource
 from flask import jsonify, request
-from playhouse.shortcuts import model_to_dict
 from ibuk.common import EBookReadSchema, Ebook
+from operator import attrgetter
 import json
 
 
-Filters = namedtuple('Filters', 'id min_size max_size date_from date_to')
-
-
 def get_filter_data(request):
-    filtering = None
+    filtering = {k:None for k in 'id min_size max_size date_from date_to'.split()}
     data = json.loads(request.data)
+    # Any key not in the list above is just ignored.
     if data and 'filters' in data:
         filters = data['filters']
-        id = int(filters['id']) if 'id' in filters else None
-        max_size = int(filters['max_size']) if 'max_size' in filters else None
-        min_size = int(filters['min_size']) if 'min_size' in filters else None
-        date_from = filters['date_from'] if 'date_from' in filters else None
-        date_to= filters['date_to'] if 'date_to' in filters else None
-        filtering = Filters(id=id, max_size=max_size, min_size=min_size, date_from=date_from, date_to=date_to)
+        for key in filters:
+            if key in filtering:
+                filtering[key] = filters[key]
     return filtering
+
+
+def select_with_filters(filters):
+    query = Ebook.select()
+    for key in filters:
+        if filters[key]:
+            query = query.where( attrgetter(key)(Ebook) == filters[key])
+    return query
+
 
 
 class EbooksResource(Resource):
@@ -42,13 +46,10 @@ class EbooksResource(Resource):
         :return: json representation of the queryset in the database.
         """
         filters = get_filter_data(request)
-        query = Ebook.select()
-        if filters:
-            if filters.id:
-                query = query.where( Ebook.id  == filters.id )
+        query = select_with_filters(filters)
         if not query.count():
             return 404
-        
+
         return {'ebooks': EBookReadSchema(many=True, only=('name','description','filename','size')).dump(query).data}
 
 
